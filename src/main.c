f6ce674090fcc4abbe662c96683d1ead64768dd9
AdrienRCQ <adrien.ricque@hotmail.com>
2022-11-15 17:41:51 +0100

    push

--- src/main.c
+++ src/main.c
@@ -10,20 +10,12 @@ int random = rand() % 100 + 1;
 
 FILE* fichier = NULL;
 fichier = fopen("..\\include\\toto.txt", "r+");  //du coup le chemin du fichier va changer mdr
-int caractereActuel = 0;
 
 if (fichier != NULL){
 
     //on peut lire et écrire dans le fichier
     printf("le fichier existe bien vu! \n");
     fprintf(fichier, random);
-        // Boucle de lecture des caractères un à un
-        do
-        {
-            caractereActuel = fgetc(fichier); // On lit le caractère
-            printf("%c", caractereActuel); // On l'affiche
-        } while (caractereActuel != EOF); // On continue tant que fgetc n'a pas retourné EOF (fin de fichier)
-
 
     fclose(fichier);
 }
